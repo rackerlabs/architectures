c8ef2c7eaaa694b7e1e7a9e594e46c86d94372f6
Jamie Hannaford <jamie.hannaford@rackspace.com>
2015-06-26 13:25:12 +0200

    Split out and organise

--- web-app/ansible/files/create_image.py
+++ web-app/ansible/files/create_image.py
@@ -1,13 +1,14 @@
 import pyrax
 import yaml
 import sys
+import os
 
 config_yml = yaml.load(open("vars.yml"))
 
 pyrax.set_setting("identity_type", "rackspace")
 pyrax.set_credentials(config_yml['username'], config_yml['api_key'])
 
-region = config_yml["region"] if config_yml["region"] else "IAD"
+region = config_yml["rs_region"] if config_yml["rs_region"] else "IAD"
 cs = pyrax.connect_to_cloudservers(region=region)
 
 img_id = None
@@ -16,16 +17,19 @@ servers = cs.servers.list()
 for server in servers:
   if server.name == 'blueprint':
     img_id = server.create_image("autoscale_blueprint")
+    break
 
 if img_id is None:
   print "No server named `blueprint`"
-  sys.Exit(1)
+  sys.exit(1)
 
 img = cs.images.get(img_id)
 img = pyrax.utils.wait_until(img, "status", ["ACTIVE", "ERROR"], attempts=0)
 
-output_file = "roles/blueprint-image/files/.img_id"
+output_file = os.path.dirname(os.path.realpath(__file__)) + "/.img_id"
 
 f = open(output_file, "w")
 f.write(img_id)
 f.close()
+
+server.delete()
--- web-app/ansible/files/setup_monitoring.py
+++ web-app/ansible/files/setup_monitoring.py
@@ -4,8 +4,6 @@ import sys
 import os
 import yaml
 
-output_file = "roles/arch-deploy/files/.np_id"
-
 try:
     opts, args = getopt.getopt(sys.argv[1:], "gud", ["group=", "up=", "down="])
 except:
@@ -24,13 +22,15 @@ for o, a in opts:
     elif o in ('-d', '--down'):
         down_policy_id = a
 
-yml = yaml.load(open('vars.yml'))
+config_yml = yaml.load(open("vars.yml"))
 
 pyrax.set_setting("identity_type", "rackspace")
-pyrax.set_credentials(yml['username'], yml['api_key'])
+pyrax.set_credentials(config_yml['username'], config_yml['api_key'])
+
+region = config_yml["rs_region"] if config_yml["rs_region"] else "IAD"
 
 cm = pyrax.cloud_monitoring
-au = pyrax.connect_to_autoscale(region=yml['region'])
+au = pyrax.connect_to_autoscale(region=region)
 
 policy = au.get(group_id)
 
@@ -62,6 +62,8 @@ ok_n = cm.create_notification("webhook", label="OK", details={"url": down_hook_l
 
 # Create notification plan
 plan = cm.create_notification_plan(label="production", ok_state=ok_n, critical_state=cr_n)
+
+output_file = os.path.dirname(os.path.realpath(__file__)) + "/.np_id"
 
 f = open(output_file, 'w')
 f.write(plan.id)
